65d22aca8b65d8a076578498921755ab581de9ef
YinHk <ministudiowingyin@gmail.com>
2022-10-17 22:44:35 -0400

    feat: update landing screen

--- src/screens/login/landing.jsx
+++ src/screens/login/landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Button } from "@rneui/themed";
 import { styled } from 'nativewind';
 import Logo from '../../assets/logo.svg';
 
@@ -8,19 +9,21 @@ const StyledText = styled(Text)
 
 const Landing = () => {
     return (
-       <StyledView tw='bg-blue-bright w-full h-full justify-center items-center'>
+      <StyledView tw='bg-blue-bright w-full h-full justify-center items-center'>
         <StyledText tw='font-bold text-white text-4xl font-extrabold'>Messaging</StyledText>
-        <Logo  align="center" /* viewBox='0 0 750 352' *//>
-       </StyledView> 
+        <StyledView tw='w-4/5 items-center'>
+          <Logo width='70%' viewBox='0 0 374 309' />
+        </StyledView>
+        <StyledView tw='items-start w-4/6'>
+          <StyledText tw='text-white text-xl'>Chat everywhere</StyledText>
+        </StyledView>
+        <StyledText tw='text-white text-xl w-4/6'>connect with your team and friends</StyledText>
+        <StyledView tw='mt-16'> 
+          <Button title='Start using' tw='bg-current rounded-lg' />
+        </StyledView>
+      </StyledView> 
     );
 }
 
-/* const styles = StyleSheet.create({
-    er: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
-}); */
 
 export default Landing;
